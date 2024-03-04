3328c4e8507dfeeed18ec3872065a4043f8fe6b1
kiraguvincent <157008739+kiraguvincent@users.noreply.github.com>
2024-03-04 13:39:59 +0300

    Add files via upload
    My work

---
+++ Budgetapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from Budgetapp import views
+from Budgetapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home, name='homepage'),
+    path('about/',views.about , name='about'),
+    path('form/',views.form , name='form')
+
+]
